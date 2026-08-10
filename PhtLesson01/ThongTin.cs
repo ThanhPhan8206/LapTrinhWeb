dd2274b89a2bf3c9b03c4d65fe65586658a9b35b
Phan Huu Thanh <phanhuuthanh8206@gmail.com>
2026-08-11 04:21:24 +0800

    Test_LTW

---
+++ PhtLesson01/ThongTin.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace PhtLesson01; // Cùng tên không gian với Program.cs
+
+class ThongTin
+{
+    // Bắt buộc phải có chữ 'public' ở đầu
+    public static void InThongTinKhac()
+    {
+        Console.WriteLine("Địa chỉ: Hà Nội");
+        Console.WriteLine("Sở thích: Lập trình");
+    }
+}
