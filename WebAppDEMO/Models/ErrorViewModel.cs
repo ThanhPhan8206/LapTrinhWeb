7d9afd817997e7c6d5f20e056bbdb1155a332341
Phan Huu Thanh <phanhuuthanh8206@gmail.com>
2026-08-23 18:33:32 +0700

    Lesson2

---
+++ WebAppDEMO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppDEMO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
